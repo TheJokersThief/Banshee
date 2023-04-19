a6acedb3039b5e35d3d23a5b84bc628f66086fd5
Evan Smith <me@iamevan.me>
2023-04-19 11:22:02 +0100

    Add a basic CLI layout for the future

--- cmd/banshee/main.go
+++ cmd/banshee/main.go
@@ -2,22 +2,60 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path"
 
+	"github.com/alecthomas/kong"
 	"github.com/kkyr/fig"
-	"github.com/thejokersthief/banshee/pkg/banshee/configs"
+	"github.com/thejokersthief/banshee/pkg/configs"
 )
 
+var CLI struct {
+	Version struct{} `cmd:"" help:"Print banshee CLI version"`
+	Migrate struct {
+		MigrationFile string `arg:"" name:"path" help:"Path to migration file." type:"path"`
+	} `cmd:"" help:"Run migration"`
+
+	ConfigFile string `name:"config" short:"c" help:"Path to global CLI config" type:"path" default:"./config.yaml"`
+}
+
 func main() {
-	var err error
+	ctx := kong.Parse(&CLI)
+
 	var globalConfig configs.GlobalConfig
-	err = fig.Load(&globalConfig)
-	if err != nil {
-		fmt.Println(err)
+	globalConfig = parseConfig(globalConfig, CLI.ConfigFile, "APP")
+
+	switch ctx.Command() {
+	case "migrate <path>":
+		fmt.Println("migrating?")
+		var migrationConfig configs.MigrationConfig
+		migrationConfig = parseConfig(migrationConfig, CLI.Migrate.MigrationFile, "APP")
+
+	case "version":
+		fmt.Println("versioning")
+	default:
+		printFatalError(fmt.Errorf(ctx.Command()))
 	}
 
-	var migrationConfig configs.MigrationConfig
-	err = fig.Load(&migrationConfig)
-	if err != nil {
-		fmt.Println(err)
+}
+
+// Unmarshal a config into a datastructure we can reuse
+func parseConfig[C configs.Configs](conf C, file string, envKey string) C {
+	dir, base := getFilePieces(file)
+	configParseError := fig.Load(&conf, fig.File(base), fig.Dirs(dir), fig.UseEnv(envKey))
+	if configParseError != nil {
+		printFatalError(configParseError)
 	}
+	return conf
+}
+
+// Break down the file pieces into the directory and filename
+func getFilePieces(filepath string) (string, string) {
+	return path.Dir(filepath), path.Base(filepath)
+}
+
+// Print a big red error message and exit
+func printFatalError(err error) {
+	fmt.Println(FatalErrorStyling.Render(err.Error()))
+	os.Exit(1)
 }
--- go.mod
+++ go.mod
@@ -3,17 +3,27 @@ module github.com/thejokersthief/banshee
 go 1.19
 
 require (
+	github.com/alecthomas/kong v0.7.1
+	github.com/charmbracelet/lipgloss v0.7.1
 	github.com/kkyr/fig v0.3.1
 	github.com/stretchr/testify v1.8.2
 )
 
 require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.1 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
