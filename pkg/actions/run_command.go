b5966fb7bf6401e2d876eb6a8cec4b8961771e19
Evan Smith <me@iamevan.me>
2023-04-27 20:31:54 +0100

    Fix command running and pipe them through bash

--- pkg/actions/run_command.go
+++ pkg/actions/run_command.go
@@ -2,11 +2,13 @@
 package actions
 
 import (
+	"fmt"
 	"os/exec"
-	"strings"
 
 	"github.com/sirupsen/logrus"
 )
+
+const defaultShell = "bash"
 
 type RunCommand struct {
 	BaseDir string
@@ -21,14 +23,14 @@ func NewRunCommandAction(dir string, description string, input map[string]string
 }
 
 func (r *RunCommand) Run(log *logrus.Entry) error {
-	commandPieces := strings.Split(r.Command, " ")
-	command := commandPieces[0]
-	args := commandPieces[1 : len(commandPieces)-1]
+	log.Debug("Running ", defaultShell, " -c `", r.Command, "`")
 
-	cmd := exec.Command(command, args...)
+	cmd := exec.Command(defaultShell, "-c", r.Command)
+	cmd.Stdout = log.WriterLevel(logrus.DebugLevel)
+	cmd.Stderr = log.WriterLevel(logrus.ErrorLevel)
 	cmd.Dir = r.BaseDir
 	if err := cmd.Run(); err != nil {
-		return err
+		return fmt.Errorf("Failed running `%s`: %s", r.Command, err)
 	}
 
 	return nil
