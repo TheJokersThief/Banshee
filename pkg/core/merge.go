057bca16764fdaecd0c3e66679f6c98422f229bf
Evan Smith <me@iamevan.me>
2023-05-12 19:46:37 +0100

    Add a function for merging approved PRs

---
+++ pkg/core/merge.go
@@ -0,0 +1,32 @@
+package core
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	openState = "open"
+)
+
+// Perform a migration
+func (b *Banshee) MergeApproved() error {
+
+	b.log = logrus.WithField("command", "migrate")
+
+	if validationErr := b.validateMigrateCommand(); validationErr != nil {
+		return validationErr
+	}
+
+	query := b.formatPRQuery(openState)
+	b.log.Debug("Getting list of PRs matching: \"", query, "\"")
+	// prList, prListErr := b.GithubClient.GetMatchingPRs(query)
+	// if prListErr != nil {
+	// 	return prListErr
+	// }
+
+	// for _, pr := range prList {
+	// 	if *pr.Mergeable && *pr.Stat
+	// }
+
+	return nil
+}
