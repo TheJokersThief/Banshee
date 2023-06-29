c7b07861cc372742c9bb837fd410124956b60074
Evan Smith <me@iamevan.me>
2023-06-29 15:13:33 +0100

    Fix bug where search query never returned any repos

--- pkg/core/migrate.go
+++ pkg/core/migrate.go
@@ -82,9 +82,8 @@ func (b *Banshee) migrationOptions() (string, []string, error) {
 			query = fmt.Sprintf("org:%s %s", org, b.MigrationConfig.SearchQuery)
 		}
 
-		if repos, searchQueryErr := b.GithubClient.GetMatchingRepos(query); searchQueryErr != nil {
-			return org, b.saveRepos(repos), searchQueryErr
-		}
+		repos, searchQueryErr := b.GithubClient.GetMatchingRepos(query)
+		return org, b.saveRepos(repos), searchQueryErr
 	}
 
 	if b.MigrationConfig.AllReposInOrg {
